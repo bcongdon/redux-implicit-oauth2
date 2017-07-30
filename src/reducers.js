9b687f32be20bb41b0cfd265187189072736cb63
Benjamin Congdon <bencongdon96@gmail.com>
2017-07-30 13:15:11 -0700

    Added isAuthenticating flag to state

--- src/reducers.js
+++ src/reducers.js
@@ -1,28 +1,35 @@
 import { hasToken, getToken, getExpiresAt } from './util/token'
-import { LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT } from './actions'
+import { LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT } from './actions'
 
 const initialState = {
   isLoggedIn: hasToken(),
   token: getToken(),
   expiresAt: getExpiresAt(),
+  isAuthenticating: false,
   error: null
 }
 
 const auth = (state = initialState, action) => {
   switch (action.type) {
+    case LOGIN_REQUEST:
+      return Object.assign({}, state, {
+        isAuthenticating: true
+      })
     case LOGIN_SUCCESS:
       return Object.assign({}, state, {
         isLoggedIn: true,
         token: action.token,
         expiresAt: action.expiresAt,
-        error: null
+        error: null,
+        isAuthenticating: false
       })
     case LOGIN_FAILURE:
       return Object.assign({}, state, {
         isLoggedIn: false,
         token: null,
         expiresAt: null,
-        error: action.error
+        error: action.error,
+        isAuthenticating: false
       })
     case LOGOUT:
       return Object.assign({}, state, {
